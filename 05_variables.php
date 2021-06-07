f7abd58cc5ef646c96f65654da0f894601908a48
Halter <halterresonmi@gmail.com>
2021-06-07 11:47:10 +0545

    sanju

---
+++ 05_variables.php
@@ -0,0 +1,12 @@
+<?php
+//variables are containers for storing information //
+//start with $//
+$name = "sanju";
+$income = 2000;
+echo "This guy is $name And His monthly income is $income<br>";
+echo "$name is a bad boy<br>";
+echo "$name is the real boy heros<br>";
+
+
+
+?>
